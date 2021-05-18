95e8f4bb83617b9f63b16bc80c6562ebc33089c5
Evandro C. Severginini <evandrosevergnini@gmail.com>
2021-05-18 12:17:19 -0300

    Prettier instalado e configurado no VS Code e no Projeto

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 const soma = (a, b) => {
   return a + b;
-}
+};
 
-const result = soma(1,3);
+const result = soma(1, 3);
 
 console.log('result', resultado);
+console.log('result', resultado);
+console.log(`result`, resultado);
