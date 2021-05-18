4bff18291b32e77af8139582f8241f1f95493bef
Evandro C. Severginini <evandrosevergnini@gmail.com>
2021-05-18 18:59:46 -0300

    Fazendo ESLint reportar erros de regras vindas do Prettier

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
-const soma = (a, b) => {
-  return a + b;
-};
+const soma = (a, b) => a + b
 
-const result = soma(1, 3);
+const result = soma(1, 3)
 
-console.log("result", resultado);
-console.log("result", resultado);
-console.log(`result`, resultado);
+console.log('result', result)
